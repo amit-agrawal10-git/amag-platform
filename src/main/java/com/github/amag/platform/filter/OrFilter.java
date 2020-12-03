3a4926779af06a483c0895576e0dbc9863754edd
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-12-03 09:05:56 +0530

    BackTest report bug fixes

--- src/main/java/com/github/amag/platform/filter/OrFilter.java
+++ src/main/java/com/github/amag/platform/filter/OrFilter.java
@@ -5,17 +5,20 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Data
 public class OrFilter<T extends Object> implements Filter<T> {
 
-    private final Filter<T> firstFilter, secondFilter;
+    private final List<Filter<T>> filters;
 
     @Override
     public Collection<T> evaluate(Collection<T> o, ArangoOperations arangoOperations) {
-        Collection<T> x = firstFilter.evaluate(o, arangoOperations);
-        x.addAll(secondFilter.evaluate(o,arangoOperations));
+        Collection<T> x = filters.get(0).evaluate(o, arangoOperations);
+        for (int i=1;i<filters.size();i++){
+            x.addAll(filters.get(i).evaluate(o,arangoOperations));
+        }
         return x;
     }
 }
